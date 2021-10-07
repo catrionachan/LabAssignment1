0f1e0cfcd180fbded541aff756cc6ddfc90b9738
catrionachan <cpwychan@gmail.com>
2021-10-07 11:03:34 -0400

    changes

--- StackDemo.java
+++ StackDemo.java
@@ -5,8 +5,21 @@ public class StackDemo{
        MyStack stack = new MyStack(0);
        System.out.println(stack.toString());
        String[] array = {"251167970", "Zheng", "Felix", "251135537","Chan", "Catriona"};
-       for (i)
-       push(array);
+       for (int i =0; i<array.length; i++){
+           stack.push(array[i]);
+       }
+       System.out.println(stack.toString());
+       System.out.println(stack.peek());
+       String distance = (stack.search("251135537")) +"";
+       System.out.println(distance);
+        int studentNum= Integer.parseInt(distance);
+       int lastNum = studentNum%10;
+       int firstNum = studentNum/1000000;
+       //System.out.println(lastNum);
+       //System.out.println(firstNum);
+       double avg = ((double)firstNum+lastNum)/2;
+       //System.out.println(avg);
+
 
 
        
